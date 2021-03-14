1586b3b2423b14e3ef3470cdf71e95b3f8d2295b
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2021-03-14 17:22:51 +0530

    about page in progress

---
+++ src/compount/card/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './card.style.css';
+
+//import gokul from "../../asserts/gokul.jpg";
+
+
+const card = () => {
+    return (
+        <div className='card'>
+            {/* <img src={gokul} alt='gokul' /> */}
+            <h2 className='ttt'>hello</h2>
+        </div>
+    )
+}
+
+export default card;
--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -2,9 +2,25 @@ import React from "react";
 
 import '../about/about.style.css';
 
+import gokul from "../../asserts/gokul.jpg";
+
+//import card from '../../compount/card/card';
+
 const about = () => {
     return (
-        <h1 className='about'>about</h1>
+        <div className='about'>
+            <div className='card-container'>
+                <div className='card'>
+                    <img src={gokul} alt='gokul' className='gokulpic' />
+                    <p className='imgtext'>gokul</p>
+                </div>
+                <div className='card2'>
+                    <p className="textph">leorm50</p>
+                </div>
+            </div>
+            
+        </div>
+        
     )
 };
 
